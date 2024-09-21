387c81ada12be8182e42f80f6c2f13fc6f36ab3b
Lex Borisoff <lex@borisoff.dev>
2024-09-21 13:09:51 -0400

    improved dirCreate test

--- test/operations/test-dir-create.ts
+++ test/operations/test-dir-create.ts
@@ -6,11 +6,37 @@ import { FileManager } from '../../src/file-manager.js';
 export function testDirCreate(testRoot: string): void {
   test('$dirCreate', () => {
     const fileManager = new FileManager();
-    const { create, files } = fileManager.root(testRoot).tree();
+    const { create, files } = fileManager.root(testRoot).tree({
+      dir1: {
+        type: 'dir',
+        children: {
+          dir1: {
+            type: 'dir',
+          },
+        },
+      },
+    });
+
     create();
 
-    files.$dirCreate('dir1');
-    const dir1 = path.join(testRoot, 'dir1');
-    expect(fs.existsSync(dir1)).toBe(true);
+    // create new dir at root level
+    files.$dirCreate('testDir1');
+    const testDir1 = path.join(testRoot, 'testDir1');
+
+    // create new dir at root/dir1 level
+    files.dir1.$dirCreate('testDir2');
+    const testDir2 = path.join(testRoot, 'dir1', 'testDir2');
+
+    // create new dir at root/dir1/dir1 level
+    files.dir1.dir1.$dirCreate('testDir3');
+    const testDir3 = path.join(testRoot, 'dir1', 'dir1', 'testDir3');
+
+    expect(fs.existsSync(testDir1)).toBe(true);
+    expect(fs.existsSync(testDir2)).toBe(true);
+    expect(fs.existsSync(testDir3)).toBe(true);
+
+    expect(fs.statSync(testDir1).isDirectory()).toBe(true);
+    expect(fs.statSync(testDir2).isDirectory()).toBe(true);
+    expect(fs.statSync(testDir3).isDirectory()).toBe(true);
   });
 }
