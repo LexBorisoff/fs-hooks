9e80823975966a22df9f44123bf6c7fdee1555cc
Lex Borisoff <lex@borisoff.dev>
2024-10-05 12:49:10 -0400

    refactored createDir test into smaller unit tests

--- test/utils/create-dir.test.ts
+++ test/utils/create-dir.test.ts
@@ -1,33 +1,55 @@
 import fs from 'node:fs';
-import { test, expect, beforeAll } from 'vitest';
+import { expect, beforeAll, suite, describe, it, afterEach } from 'vitest';
 import { createDir } from '../../src/utils/create-dir.js';
 import { testSetup } from '../test-setup.js';
 
-const { setup, joinPath } = testSetup('create-dir', import.meta);
+const { testPath, setup, joinPath } = testSetup('create-dir', import.meta);
 
-beforeAll(() => {
-  return setup();
-});
+suite('createDir Suite', { concurrent: false, sequential: true }, () => {
+  beforeAll(() => {
+    return setup();
+  });
 
-test('createDir', () => {
-  // attemp to create the directory with recursive flag set to false
-  let dirPath: string = joinPath('dir1', 'dir2');
-  expect(() => createDir(dirPath, false)).toThrow();
+  describe('createDir function', () => {
+    let dirPath: string | undefined;
 
-  // create a directory
-  dirPath = joinPath('dir1');
-  createDir(dirPath);
-  expect(fs.existsSync(dirPath)).toBe(true);
-  fs.rmSync(dirPath, { force: true, recursive: true });
-  expect(fs.existsSync(dirPath)).toBe(false);
+    afterEach(() => {
+      const files = fs.readdirSync(testPath);
+      files.forEach((file) => {
+        fs.rmSync(joinPath(file), {
+          force: true,
+          recursive: true,
+        });
+      });
+    });
 
-  // create a nested directory
-  dirPath = joinPath('dir1', 'dir2');
-  createDir(dirPath);
-  expect(fs.existsSync(dirPath)).toBe(true);
+    it('should create a directory', () => {
+      dirPath = joinPath('dir1');
+      expect(fs.existsSync(dirPath)).toBe(false);
+      createDir(dirPath);
+      expect(fs.existsSync(dirPath)).toBe(true);
+    });
 
-  // attempt creating a directory at the existing path that is not a directory
-  dirPath = joinPath('file1');
-  fs.writeFileSync(dirPath, '');
-  expect(() => createDir(dirPath)).toThrow();
+    it('should create a nested directory', () => {
+      dirPath = joinPath('dir1', 'dir2');
+      expect(fs.existsSync(dirPath)).toBe(false);
+      createDir(dirPath);
+      expect(fs.existsSync(dirPath)).toBe(true);
+    });
+
+    it('should throw when recursive flag is false for creating a nested directory', () => {
+      expect(() => {
+        dirPath = joinPath('dir1', 'dir2');
+        createDir(dirPath, false);
+      }).toThrow();
+    });
+
+    it('should throw when creating a directory at an existing file path', () => {
+      expect(() => {
+        dirPath = joinPath('file1');
+        fs.writeFileSync(dirPath, '');
+        createDir(dirPath);
+      }).toThrow();
+    });
+  });
 });
