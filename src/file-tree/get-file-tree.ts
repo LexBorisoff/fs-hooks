214295262b448eeb72e069567ded771537827ae4
Lex Borisoff <lex@borisoff.dev>
2024-10-30 22:36:13 -0400

    deleted getFileTree and its test

--- src/file-tree/get-file-tree.ts
+++
@@ -1,8 +0,0 @@
-import type { FileTreeInterface } from './file-tree.types.js';
-
-/**
- * Identity function to create a file tree
- */
-export function getFileTree<T extends FileTreeInterface>(tree: T): T {
-  return tree;
-}
