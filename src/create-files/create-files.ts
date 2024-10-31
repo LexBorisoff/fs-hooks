1f0fe2c2e72654188516dc2f909e4b75e39cdb8d
Lex Borisoff <lex@borisoff.dev>
2024-10-30 21:40:57 -0400

    feat: createFile function to create files based on operation tree

---
+++ src/create-files/create-files.ts
@@ -0,0 +1,94 @@
+import fs from 'node:fs';
+import type { FileTreeInterface } from '../file-tree/file-tree.types.js';
+import type {
+  OperationsType,
+  RootOperationTreeType,
+} from '../operations/operation.types.js';
+import { isDirectory } from '../utils/is-directory.js';
+import { getFullPath } from '../file-tree/get-full-path.js';
+import { createDir } from '../utils/create-dir.js';
+import { HIDDEN_PROPERTIES } from '../operations/build-operation-tree.js';
+
+function logErrors(errors: string[]): void {
+  errors.forEach((error) => {
+    console.error(error);
+  });
+}
+
+export function createFiles(
+  operationTree: RootOperationTreeType<
+    FileTreeInterface,
+    OperationsType,
+    OperationsType
+  >,
+): void {
+  const rootPath = operationTree.$getPath();
+
+  if (fs.existsSync(rootPath) && !isDirectory(rootPath)) {
+    throw new Error('Root path already exists and is not a directory');
+  }
+
+  const errors: string[] = [];
+
+  function addError(type: 'dir' | 'file', filePath: string): void {
+    errors.push(
+      `Cannot create ${type === 'file' ? 'file' : 'directory'} ${filePath}`,
+    );
+  }
+
+  function createFilesRecursively(
+    parentPath: string,
+    currentFileTree?: FileTreeInterface,
+  ): void {
+    Object.entries(currentFileTree ?? {}).forEach(([key, value]) => {
+      const fullPath = getFullPath(parentPath, key);
+
+      if (typeof value === 'string') {
+        if (fs.existsSync(fullPath) && isDirectory(fullPath)) {
+          addError('file', fullPath);
+          return;
+        }
+
+        fs.writeFileSync(fullPath, value);
+        return;
+      }
+
+      try {
+        createDir(fullPath);
+
+        if (Object.keys(value).length > 0) {
+          createFilesRecursively(fullPath, value);
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          errors.push(error.message);
+        } else {
+          addError('dir', fullPath);
+        }
+      }
+    });
+  }
+
+  try {
+    if (!fs.existsSync(rootPath)) {
+      createDir(rootPath);
+    }
+
+    const fileTree: FileTreeInterface = Object.getOwnPropertyDescriptor(
+      operationTree,
+      HIDDEN_PROPERTIES.Tree,
+    )?.value;
+
+    createFilesRecursively(rootPath, fileTree);
+  } catch (error) {
+    if (error instanceof Error) {
+      errors.push(error.message);
+    } else {
+      addError('dir', rootPath);
+    }
+  }
+
+  if (errors.length > 0) {
+    logErrors(errors);
+  }
+}
