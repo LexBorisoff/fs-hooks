4dc36abed88cd160f3f7c68ca98eed1a0acf4b02
Lex Borisoff <lex@borisoff.dev>
2024-11-13 22:13:01 -0500

    test: added test coverage script

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      exclude: ['**/*types.ts', ...coverageConfigDefaults.exclude],
+    },
+  },
 });
