e89afb7cf7c03e592d0ff93d5868fcab56701cb2
Lex Borisoff <lex@borisoff.dev>
2024-11-13 19:59:20 -0500

    chore: removed utils folder from being one of project exports

--- src/utils/parse-data.ts
+++ src/utils/parse-data.ts
@@ -2,7 +2,9 @@
  * Returns parsed data of Result type
  * or null if data string is empty or cannot be parsed
  */
-export function parseData<Result = any>(data?: string | null): Result | null {
+export function parseData<Result = unknown>(
+  data?: string | null,
+): Result | null {
   if (data == null || data === '') {
     return null;
   }
