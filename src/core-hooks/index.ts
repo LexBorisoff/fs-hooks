4663c270a1fa957b93f06437b22caf2d4c25f19e
Lex Borisoff <lex@borisoff.dev>
2025-01-18 20:10:13 -0500

    refactor: moved core hooks types to core-hooks folder

--- src/core-hooks/index.ts
+++ src/core-hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './core-hooks.js';
+export * from './core-hooks.types.js';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,2 @@
-export * from './core-hooks.types.js';
 export * from './hook.types.js';
 export * from './tree.types.js';
